3e261ac9ca36587affcb58513a7f95c9307cc579
Justin Ellis <jdellis490@gmail.com>
2022-05-19 12:42:56 -0700

    Code to generate HTML for team

--- index.js
+++ index.js
@@ -132,5 +132,67 @@ function newIntern() {
     })
 };
 
+// Generating HTML for employees on team
+function formTeam() {
+    const generateHTML = []
+    const beginHTML =
+
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header class= "header"><b>${createdTeam[0]}</b></header>
+`
+    generateHTML.push(beginHTML);
+    for (let i = 1; i < createdTeam.length; i++) {
+        
+        let employeeInfo = `
+        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+        <div class="card-header"><br> ${createdTeam[i].role} </br></div>
+        <div class="card-body">
+        <h5 class="card-title"> ${createdTeam[i].name} </h5>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${createdTeam[i].id} </li>
+            <li class="list-group-item">Email: <a href="mailto:${createdTeam[i].email}">${createdTeam[i].email}</a></li>
+`
+        if (createdTeam[i].officenumber){
+            employeeInfo += `
+            <li class="list-group-item">Office Number: ${createdTeam[i].officenumber} </li>
+        </ul>`
+        } else if (createdTeam[i].github) {
+            employeeInfo += `
+            <li class="list-group-item">GitHub: <a href="https://github.com/${createdTeam[i].github}">${createdTeam[i].github}</a></li>
+        </ul>`
+        } else if (createdTeam[i].school){
+            employeeInfo += `
+            <li class="list-group-item">School Name: ${createdTeam[i].school} </li>
+        </ul>`
+        } employeeInfo += `
+            </div>
+            </div>
+            `
+        generateHTML.push(employeeInfo);
+    }
+`
+</body>
+</html>`
+
+// Write to File function to make HTML page
+fs.writeFile(`.dist/team.html`, generateHTML.join(""), (error) => {
+    if (error) throw error;
+    console.log('HTML has been written.');
+});
+
+}
 teamPrompt();
 
